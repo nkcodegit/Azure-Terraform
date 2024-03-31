7f329f11466f58b6753ce400accd4ac9b174b1ac
NKCODE <46096557+nkcodegit@users.noreply.github.com>
2024-03-31 11:28:44 +0800

    Create main.tf

---
+++ VNET/azure_vnet/main.tf
@@ -0,0 +1,12 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = var.name
+  address_space       = var.address_space
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  tags = var.tags
+}
